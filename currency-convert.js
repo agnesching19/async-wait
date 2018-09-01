be4a5a40c802e308b9381ccb18f9505dbf9ef2bc
Agnes Ching <agnesching19@gmail.com>
2018-09-01 15:28:15 +0100

    handling Errors and Awaiting Async function

--- currency-convert.js
+++ currency-convert.js
@@ -9,11 +9,16 @@ const axios = require('axios');
 // };
 
 const getExchangeRate = async (from, to) => {
-  const response = await axios.get('http://data.fixer.io/api/latest?access_key=446095608a105d4303a6158be5a31c1f');
-  const euro = 1 / response.data.rates[from];
-  const rate = euro * response.data.rates[to];
-  
-  return rate;
+  try {
+    const response = await axios.get('http://data.fixer.io/api/latest?access_key=446095608a105d4303a6158be5a31c1f');
+    const euro = 1 / response.data.rates[from];
+    const rate = euro * response.data.rates[to];
+
+    if (isNaN(rate)) throw new Error();
+    return rate;
+  } catch (e) {
+    throw new Error(`Unable to get exchange rate for ${from} and ${to}.`);
+  }
 };
 
 // const getCountries = (currencyCode) => {
@@ -23,9 +28,12 @@ const getExchangeRate = async (from, to) => {
 // };
 
 const getCountries = async (currencyCode) => {
-  const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
-  
-  return response.data.map((country) => country.name);
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+    return response.data.map((country) => country.name);
+  } catch (e) {
+    throw new Error(`Unable to get countries that use ${currencyCode}`);
+  }
 };
 
 // const convertCurrecy = (from, to , amount) => {
@@ -55,4 +63,23 @@ const convertCurrecy = async (from, to , amount) => {
 
 convertCurrecy('GBP', 'EUR', 100).then((message) => {
   console.log(message);
+}).catch((e) => {
+  console.log(e.message);
+});
+
+const add = async (a, b) => a + b + c;
+
+const doWork = async () => {
+  try {
+    const result = await add(19, 5);
+    return result;
+  } catch (e) {
+    return 10;
+  }
+};
+
+doWork().then((data) => {
+  console.log(data);
+}).catch((e) => {
+  console.log('Oops! Something went wrong.');
 });
