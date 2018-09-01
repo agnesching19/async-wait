03bab00af7cff7dc39811f823dd29a186408017a
Agnes Ching <agnesching19@gmail.com>
2018-09-01 15:13:23 +0100

    Async/Await real world example II

--- currency-convert.js
+++ currency-convert.js
@@ -1,6 +1,3 @@
-// GBP, TWD, 50 
-// http://data.fixer.io/api/latest?access_key=446095608a105d4303a6158be5a31c1f
-
 const axios = require('axios');
 
 // const getExchangeRate = (from, to) => {
@@ -31,10 +28,31 @@ const getCountries = async (currencyCode) => {
   return response.data.map((country) => country.name);
 };
 
-getExchangeRate('GBP', 'TWD').then((rate) => {
-  console.log(rate);
-});
+// const convertCurrecy = (from, to , amount) => {
+//   let convertedAmount;
+//   return getExchangeRate(from, to).then((rate) => {
+//     convertedAmount = (amount * rate).toFixed(2);
+//     return getCountries(to);
+//   }).then((countries) => {
+//     return `${amount} ${from} is worth ${convertedAmount} ${to}. You can spend it in the following countries: ${countries.join(', ')}.`;
+//   });
+// }
 
-getCountries('EUR').then((country) => {
-  console.log(country);
+const convertCurrecy = async (from, to , amount) => {
+  const convertedAmount = await getExchangeRate(from, to).then((rate) => (amount * rate).toFixed(2));
+  const countries = await getCountries(to);
+  
+  return `${amount} ${from} is worth ${convertedAmount} ${to}. You can spend it in the following countries: ${countries.join(', ')}.`;
+}
+
+// getExchangeRate('GBP', 'TWD').then((rate) => {
+//   console.log(rate);
+// });
+
+// getCountries('EUR').then((country) => {
+//   console.log(country);
+// });
+
+convertCurrecy('GBP', 'EUR', 100).then((message) => {
+  console.log(message);
 });
